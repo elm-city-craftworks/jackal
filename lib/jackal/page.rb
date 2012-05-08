b4269a32e5017031c42eae0a8d090136ec8850b4
Gregory Brown <gregory.t.brown@gmail.com>
2012-05-08 16:44:14 -0400

    Page model is working

--- lib/jackal/page.rb
+++ lib/jackal/page.rb
@@ -1,15 +1,28 @@
+require "yaml"
+
 module Jackal
   class Page
     def initialize(source_file)
-      @source_file = source_file
+      @filename = File.basename(source_file)
+
+      read_page(source_file)
     end
 
-    def filename
-      File.basename(source_file)
-    end
+    attr_reader :content, :filename, :metadata, :contents
 
     private
 
-    attr_reader :source_file
+    def read_page(filename)
+      @contents = File.read(filename)
+
+      begin
+        if (md = @contents.match(/^(?<headers>---\s*\n.*?\n?)^(---\s*$\n?)/m))
+          @contents = md.post_match
+          @metadata = YAML.load(md[:headers])
+        end
+      rescue => e
+        puts "YAML Exception reading #{filename}: #{e.message}"
+      end
+    end
   end
 end
--- test/units/page_test.rb
+++ test/units/page_test.rb
@@ -11,4 +11,13 @@ describe Jackal::Page do
   it "must extract the base filename" do
     page.filename.must_equal("2012-05-07-first-post.markdown")
   end
+
+  it "must extract the metadata" do
+    page.metadata.must_equal("title"    => "First Post",
+                             "category" => "essays")
+  end
+
+  it "must extract the contents" do
+    page.contents.must_equal("My First Post!\n")
+  end
 end
